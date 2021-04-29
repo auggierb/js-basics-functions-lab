29621e2d3f1baa57fb7d2be8477092063bb2f97e
August <auggierutter@gmail.com>
2021-04-29 19:38:50 -0400

    commit tree 7b352300d6fcf13b42570ce3982892d858567efc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const block = 264
+function distanceFromHqInBlocks(value){
+if(42 < value){return value - 42}
+else{return 42 - value} 
+}
+
+function distanceFromHqInFeet(value){
+    return distanceFromHqInBlocks(value) * block
+} 
+
+function distanceTravelledInFeet(a, b){
+    if(a < b){return (b - a) * block}
+    else{return (a - b) * block}
+}
+
+function calculatesFarePrice(start , destination) {
+    let length = distanceTravelledInFeet(start, destination)
+    if (length < 401) {return 0}
+    else if(length > 401 && length  <= 2000) {return (length - 400) * 0.02}
+    else if(length > 2500) {return 'cannot travel that far'}
+    else {return 25}
+}
+    
